737f9eb4183c62d922dd9fc577ad938ab0befe13
aaronzalez <70921164+aaronzalez@users.noreply.github.com>
2022-08-28 20:00:30 -0400

    social media added in Contact Us

--- pages/Contact.js
+++ pages/Contact.js
@@ -4,8 +4,26 @@ const Contact = () => {
     return (
         <div className={styles.Body}>
             <h1>Primary Contact: </h1>
-            <br></br>
             <p className={styles.email}>kliendo@purdue.edu</p>
+            <br></br>
+            <h2>Get Invovled</h2>
+            <div className={styles.Box}>
+                <div className={styles.socialsImages}>
+                    <a href="https://discord.gg/AjEMaJ2">
+                        <img src="/discord.png" />
+                    </a>
+                    <a href="https://www.instagram.com/purduedota2/">
+                        <img src="/instagram.png" />
+                    </a>
+                </div>
+            </div>
+            <div className={styles.Box}>
+                <div className={styles.socials}>
+                    <p>Discord</p>
+                    <p>Instagram</p>
+                </div>
+            </div>
+           
         </div>
     )
 }
